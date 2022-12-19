5d4be93ff9b9b9bb420df5a364fda9081b07e539
Артём Коваленко <ak7527203@gmail.com>
2022-12-19 22:33:47 +0200

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 0;
+        for (int i = 0; i <=12; i++)
+        {
+            for(int j = 0; j <= 59; j++)
+            {
+                if (i == j * 5 && i > 0) { Console.WriteLine($"Часовая и минутная стрелка совпадают на времени {i}:{j}"); n++; break; }
+            }
+            if (n == 1) break;
+        }
+
+        n = 0;
+        for (int i = 0; i <= 12; i++)
+        {
+            for (int j = 0; j <= 59; j++)
+            {
+                if (i * 20 == j && i > 0) { Console.WriteLine($"Часовая и минутная стрелка перпендикулярны на времени {i}:{j}"); n++; break; }
+            }
+            if (n == 1) break;
+        }
+    }
+}
